6c6a7ca166e8025873cc79648d357da758f25f41
Nguyễn Thị Hồng Thơ <22151305@student.hcmute.edu.vn>
2025-05-02 18:36:55 +0700

    add ignore

--- obj/Debug/net8.0-windows/GameNinjaSchool_GK.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/GameNinjaSchool_GK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameNinjaSchool_GK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9998825614b9db091cf1466b98b0221a044e83d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d3e49a9343d442ab98671ce811eefe37f5480f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameNinjaSchool_GK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameNinjaSchool_GK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
