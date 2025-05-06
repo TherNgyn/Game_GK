dfe4d14e4926ef2e2eaac2103932c197636db6e5
Nguyễn Thị Hồng Thơ <22151305@student.hcmute.edu.vn>
2025-05-06 16:20:52 +0700

    merge Hao1

--- obj/Debug/net8.0-windows/GameNinjaSchool_GK.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/GameNinjaSchool_GK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameNinjaSchool_GK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6a7ca166e8025873cc79648d357da758f25f41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+601a5ac9ddef45f58a3ff4cb058cfa2ccb917317")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameNinjaSchool_GK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameNinjaSchool_GK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
