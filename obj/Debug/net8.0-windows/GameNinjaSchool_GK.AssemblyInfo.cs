1097a00df46f6d2e749fcadb27b8402867e2c0b9
Nguyễn Thị Hồng Thơ <22151305@student.hcmute.edu.vn>
2025-05-07 20:37:21 +0700

    fix conflict

--- obj/Debug/net8.0-windows/GameNinjaSchool_GK.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/GameNinjaSchool_GK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameNinjaSchool_GK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6beab67f204f404515103514848c7fbe974fe2be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76ec0ad85b94163054d1d16fec6f6d877077b57")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameNinjaSchool_GK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameNinjaSchool_GK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
